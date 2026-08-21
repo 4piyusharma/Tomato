c34d307fe87512f2149603482d463fdec2739ae9
Piyush Sharma <88895440+4piyusharma@users.noreply.github.com>
2026-08-21 23:54:13 +0530

    Update service URLs for production environment
    Uncommented service URLs for production use and commented out local URLs.

--- src/main.tsx
+++ src/main.tsx
@@ -7,20 +7,20 @@ import { AppProvider } from "./context/AppContext.tsx";
 import "leaflet/dist/leaflet.css";
 import { SocketProvider } from "./context/SocketContext.tsx";
 
-// export const authService = "https://tomatos-service.onrender.com";
-// export const restaurantService = "https://restaurants-service-1.onrender.com";
-// export const utilsService = "https://utilsss-service.onrender.com";
-// export const realtimeService = "https://realtimes-service-m3j7.onrender.com";
-// export const riderService = "https://riders-service-1.onrender.com";
-// export const adminService = "https://admins-service-1.onrender.com";
+export const authService = "https://tomatos-service.onrender.com";
+export const restaurantService = "https://restaurants-service-1.onrender.com";
+export const utilsService = "https://utilsss-service.onrender.com";
+export const realtimeService = "https://realtimes-service-m3j7.onrender.com";
+export const riderService = "https://riders-service-1.onrender.com";
+export const adminService = "https://admins-service-1.onrender.com";
 
 
-export const authService = "http://localhost:5000"; 
-export const restaurantService = "http://localhost:5001";
-export const utilsService = "http://localhost:5002";
-export const realtimeService = "http://localhost:5004";
-export const riderService = "http://localhost:5005";
-export const adminService = "http://localhost:5006";
+// export const authService = "http://localhost:5000"; 
+// export const restaurantService = "http://localhost:5001";
+// export const utilsService = "http://localhost:5002";
+// export const realtimeService = "http://localhost:5004";
+// export const riderService = "http://localhost:5005";
+// export const adminService = "http://localhost:5006";
 
 
 createRoot(document.getElementById("root")!).render(
